eb6d98fbab79852763f673c6978c42ac5b66b20c
Yutaka <y.tajika529@gmail.com>
2022-12-02 08:24:30 +0900

    refactored: Package.swift

--- Packages/Package.swift
+++ Packages/Package.swift
@@ -13,39 +13,25 @@ let configuration: Configuration = {
     return configuration
 }()
 
-var swiftSettings: [SwiftSetting] = []
-
-switch configuration {
-case .debug:
-    swiftSettings.append(.define("DEBUG"))
-case .adhoc:
-    swiftSettings.append(.define("AD_HOC"))
-case .release:
-    swiftSettings.append(.define("RELEASE"))
-}
-
 let package = Package(
     name: "Packages",
     platforms: [
         .iOS(.v14),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AppFeature",
             targets: ["AppFeature"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AppFeature",
             dependencies: [],
-            swiftSettings: swiftSettings),
+            swiftSettings: [
+                configuration.swiftSetting
+            ]),
     ]
 )
 
@@ -53,4 +39,8 @@ enum Configuration: String {
     case debug = "DEBUG"
     case release = "RELEASE"
     case adhoc = "AD_HOC"
+    
+    var swiftSetting: SwiftSetting {
+        .define(self.rawValue)
+    }
 }
